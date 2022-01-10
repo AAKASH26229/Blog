51d6140328312a96ef51593e25bd6fd570a3fdb7
bobby <parateakash21.ap@gmail.com>
2022-01-10 15:39:48 +0530

    Added some templates files

--- blog/urls.py
+++ blog/urls.py
@@ -2,10 +2,23 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from posts.views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeletView
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('', PostListView.as_view(), name='list'),
+    path('<slug>/', PostDetailView.as_view(), name='detail'),
+    path('<slug>/update/', PostUpdateView.as_view(), name='update'),
+    path('<slug>/delete/', PostDeletView.as_view(), name='delete'),
+    path('create/', PostCreateView.as_view(), name='create'),
 ]
 
 if settings.DEBUG:
